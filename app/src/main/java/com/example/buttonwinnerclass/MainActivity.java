ce25f33786cab95acc7691912f126363ff38124a
JJW <ckjjw2953@naver.com>
2021-03-30 10:39:05 +0900

    inner class with this

--- app/src/main/java/com/example/buttonwinnerclass/MainActivity.java
+++ app/src/main/java/com/example/buttonwinnerclass/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.buttonwinnerclass;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -19,5 +20,17 @@ public class MainActivity extends AppCompatActivity {
         mTextView1 = findViewById(R.id.textView1);
 
         mButton1.setOnClickListener(new MyOnClickListener(this));
+    }
+
+    private class MyOnClickListener implements View.OnClickListener {
+        MainActivity mainActivity;
+        public MyOnClickListener(MainActivity mainActivity) {
+            this.mainActivity = mainActivity;
+        }
+
+        @Override
+        public void onClick(View v) {
+            mainActivity.mTextView1.setText("You clicked 정지원's button!");
+        }
     }
 }
